13e4243c9e50cf7b8d3a2b61bddd4f9fc3d3071b
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-10-29 12:06:56 +0300

    noManager passes , made the tests cover mapping departments and teams

--- src/main/java/com/orange/hr/entity/Team.java
+++ src/main/java/com/orange/hr/entity/Team.java
@@ -3,16 +3,22 @@ package com.orange.hr.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="teams")
 public class Team {
 	@Id
 	@GeneratedValue
-	private Long Team_id;
+	private int Team_id;
 
 	private String name;
 }
--- src/test/java/com/orange/hr/unit/EmployeeServiceUnitTest.java
+++ src/test/java/com/orange/hr/unit/EmployeeServiceUnitTest.java
@@ -42,22 +42,20 @@ public class EmployeeServiceUnitTest {
     private EmployeeServiceImpl employeeService;
 
     @Test
-    public void addEmployee_givenFullValidData_shouldReturnSavedEmployee() {
+    public void addEmployee_givenValidDataWithNoExpertise_shouldReturnSavedEmployee() {
 
         //Arrange
         EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO(1, "ahmed ELdera", LocalDate.of(2003, 2, 18), Gender.MALE, LocalDate.of(2026, 4, 12), 1000, 1, 1, 1, null);
-        EmployeeResponseDTO employeeResponseDTO = new EmployeeResponseDTO(1, "ahmed ELdera", LocalDate.of(2003, 2, 18), Gender.MALE, LocalDate.of(2026, 4, 12), 1000, new DepartmentDTO(1, "dept 1"), 1, new TeamDTO(1, "team 1"), null);
+        EmployeeResponseDTO employeeResponseDTO = new EmployeeResponseDTO(1, "ahmed ELdera", LocalDate.of(2003, 2, 18), Gender.MALE, LocalDate.of(2026, 4, 12), 1000, new DepartmentDTO(1, "dept 1"), null, new TeamDTO(1, "team 1"), null);
         Employee emp = new Employee();
-        Optional<Department> department = Optional.of(new Department(1, "abc"));
-        Optional<Team> team = Optional.of(new Team(1, "abc"));
+        Optional<Department> department = Optional.of(new Department(1, "dept 1"));
+        Optional<Team> team = Optional.of(new Team(1, "team 1"));
         Optional<Employee> manager = Optional.of(new Employee());
-        manager.get().setEmployeeID(1);
-        emp.setName("ahmed Eldera");
         when(employeeMapper.toEntity(employeeRequestDTO)).thenReturn(emp);
         when(employeeRepository.save(emp)).thenReturn(emp);
         when(employeeMapper.toDTO(emp)).thenReturn(employeeResponseDTO);
-        when(departmentRepository.findById(any())).thenReturn(department);
-        when(teamRepository.findById(any())).thenReturn(team);
+        when(departmentRepository.findById(employeeRequestDTO.getDepartmentId())).thenReturn(department);
+        when(teamRepository.findById(employeeRequestDTO.getTeamId())).thenReturn(team);
         when(employeeRepository.findById(employeeRequestDTO.getManagerId())).thenReturn(manager);
 
         //act
@@ -66,18 +64,21 @@ public class EmployeeServiceUnitTest {
         //assert
         assertEquals(result.getEmployeeID(), employeeResponseDTO.getEmployeeID());
         assertEquals(result.getName(), employeeResponseDTO.getName());
+        assertEquals(result.getDepartment().getDepartment_id(), employeeResponseDTO.getDepartment().getDepartment_id());
+        assertEquals(result.getTeam().getTeam_id(), employeeResponseDTO.getTeam().getTeam_id());
+        assertEquals(result.getManagerId(), employeeResponseDTO.getManagerId());
     }
-     @Test
-    public void addEmployee_givenValidDataWithNoManager_shouldReturnSavedEmployee() {
+
+    @Test
+    public void addEmployee_givenValidDataWithNoExpertiseNoManager_shouldReturnSavedEmployee() {
 
         //Arrange
         EmployeeRequestDTO employeeRequestDTO = new EmployeeRequestDTO(1, "ahmed ELdera", LocalDate.of(2003, 2, 18), Gender.MALE, LocalDate.of(2026, 4, 12), 1000, 1, null, 1, null);
-        EmployeeResponseDTO employeeResponseDTO = new EmployeeResponseDTO(1, "ahmed ELdera", LocalDate.of(2003, 2, 18), Gender.MALE, LocalDate.of(2026, 4, 12), 1000, new DepartmentDTO(1, "dept 1"), null , new TeamDTO(1, "team 1"), null);
+        EmployeeResponseDTO employeeResponseDTO = new EmployeeResponseDTO(1, "ahmed ELdera", LocalDate.of(2003, 2, 18), Gender.MALE, LocalDate.of(2026, 4, 12), 1000, new DepartmentDTO(1, "dept 1"), null, new TeamDTO(1, "team 1"), null);
         Employee emp = new Employee();
         Optional<Department> department = Optional.of(new Department(1, "abc"));
         Optional<Team> team = Optional.of(new Team(1, "abc"));
         Optional<Employee> manager = Optional.empty();
-        emp.setName("ahmed Eldera");
         when(employeeMapper.toEntity(employeeRequestDTO)).thenReturn(emp);
         when(employeeRepository.save(emp)).thenReturn(emp);
         when(employeeMapper.toDTO(emp)).thenReturn(employeeResponseDTO);
@@ -91,5 +92,8 @@ public class EmployeeServiceUnitTest {
         //assert
         assertEquals(result.getEmployeeID(), employeeResponseDTO.getEmployeeID());
         assertEquals(result.getName(), employeeResponseDTO.getName());
+        assertEquals(result.getDepartment().getDepartment_id(), employeeResponseDTO.getDepartment().getDepartment_id());
+        assertEquals(result.getTeam().getTeam_id(), employeeResponseDTO.getTeam().getTeam_id());
+        assertEquals(result.getManagerId(), employeeResponseDTO.getManagerId());
     }
 }
