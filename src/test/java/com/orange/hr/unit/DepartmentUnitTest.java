a318729dfe2c18071c00abe5c9e08436f255527b
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-11-01 13:58:24 +0200

    test for unique departments (red)

--- src/test/java/com/orange/hr/unit/DepartmentUnitTest.java
+++ src/test/java/com/orange/hr/unit/DepartmentUnitTest.java
@@ -18,12 +18,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class DepartmentUnitTest {
     @Autowired
     private DepartmentRepository departmentRepository;
+
     @Test
-    public void CreateDepartment_WithName_ShouldReturnSavedEntity(){
-        Department dept = new Department(null,"department 1");
+    public void CreateDepartment_WithName_ShouldReturnSavedEntity() {
+        Department dept = new Department(null, "department 1");
         assertNull(dept.getDepartment_Id());
         dept = departmentRepository.save(dept);
-        assertNotEquals(dept.getDepartment_Id(),null);
-        assertEquals(dept.getName(),"department 1");
+        assertNotEquals(dept.getDepartment_Id(), null);
+        assertEquals(dept.getName(), "department 1");
+    }
+
+    @Test
+    public void CreateDepartment_WithExistingName_shouldThrowException() {
+        Department dept1 = new Department(null, "department 1");
+        dept1 = departmentRepository.save(dept1);
+        Department dept2 = new Department(null, "department 1");
+        assertThrows(Exception.class,()-> departmentRepository.save(dept2));
+
+
     }
 }
