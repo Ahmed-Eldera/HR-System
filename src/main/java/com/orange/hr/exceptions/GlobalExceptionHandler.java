8637f39a42d7ea9401b35c65a16d2faf7a1068a8
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-11-10 15:33:24 +0200

    enhanced errors for missing fields

--- src/main/java/com/orange/hr/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/orange/hr/exceptions/GlobalExceptionHandler.java
@@ -3,9 +3,12 @@ package com.orange.hr.exceptions;
 import com.orange.hr.dto.CustomError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,8 +17,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException exception) {
 
         CustomError error = new CustomError();
+        StringBuffer msg = new StringBuffer();
+        List<ObjectError> errors = exception.getBindingResult().getAllErrors();
+        for(ObjectError e: errors){
+            msg.append(e.getDefaultMessage() + " ,");
+        }
 
-        error.setMsg( exception.getBindingResult().getAllErrors().getFirst().getDefaultMessage());
+        error.setMsg(msg.toString());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(error);
