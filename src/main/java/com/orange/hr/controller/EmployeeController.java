df8168d84313327e44b65de262dd90be3edfb6e2
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-11-11 10:19:24 +0200

    fixed endpoint name

--- src/main/java/com/orange/hr/controller/EmployeeController.java
+++ src/main/java/com/orange/hr/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
