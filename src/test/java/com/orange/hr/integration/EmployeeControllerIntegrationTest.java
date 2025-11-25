2b1306264d4095eec7f0038624919de8393a4571
Ahmed-Eldera <hotoeldera@yahoo.com>
2025-11-26 01:08:45 +0300

    refactor:remove magic numbers from get salary test

--- src/test/java/com/orange/hr/integration/EmployeeControllerIntegrationTest.java
+++ src/test/java/com/orange/hr/integration/EmployeeControllerIntegrationTest.java
@@ -61,6 +61,8 @@ public class EmployeeControllerIntegrationTest extends AbstractTest {
     private static final int EXPERTISE_ID = 1;
     private static final int EXPERTISE_ID2 = 1;
     private static final int NON_EXISTENT_EXPERTISE_ID = 123;
+    private static final int INSURANCE = 500;
+    private static final float TAX =0.15f;
 
 
     @Test
@@ -513,7 +515,7 @@ public class EmployeeControllerIntegrationTest extends AbstractTest {
     public void GetSalary_WithValidEmployee_ShouldReturnOK() throws Exception {
         prepareDB("/datasets/GetSalary.xml");
         //prepare
-        double netSalary = SALARY - 500 - SALARY * 0.15; //net = gross - fixed 500 and - 15% tax
+        double netSalary = SALARY - INSURANCE - SALARY * TAX; //net = gross - fixed 500 and - 15% tax
         //act
         ResultActions result = mockMvc.perform(get("/employee/" + EXISTING_EMPLOYEE_ID + "/salary"));
 
@@ -526,7 +528,7 @@ public class EmployeeControllerIntegrationTest extends AbstractTest {
     public void GetSalary_WithInValidEmployee_ShouldReturnNotFound() throws Exception {
         prepareDB("/datasets/GetSalary.xml");
         //prepare
-        double netSalary = SALARY - 500 - SALARY * 0.15; //net = gross - fixed 500 and - 15% tax
+        double netSalary = SALARY - INSURANCE - SALARY * TAX; //net = gross - fixed 500 and - 15% tax
         //act
         ResultActions result = mockMvc.perform(get("/employee/" + NON_EXISTENT_EMPLOYEE_ID + "/salary"));
 
