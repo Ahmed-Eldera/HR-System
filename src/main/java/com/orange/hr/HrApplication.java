c94134e58a22d2c0147f91ff09c154a70a6e815f
Ahmed-Eldera <hotoeldera@yahoo.com>
2026-01-11 10:06:56 +0200

    chore: intial empty scheduled task

--- src/main/java/com/orange/hr/HrApplication.java
+++ src/main/java/com/orange/hr/HrApplication.java
@@ -2,8 +2,10 @@ package com.orange.hr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HrApplication {
 
     public static void main(String[] args) {
@@ -11,5 +13,3 @@ public class HrApplication {
     }
 
 }
-
-//testing the release bot
---
+++ src/main/java/com/orange/hr/scheduledTasks/ScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.orange.hr.scheduledTasks;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    //    @Scheduled(cron = "0 0 0 25 * * ")
+//    @Scheduled(cron = @monthly)
+    public void reportCurrentTime() {
+        log.info("hr now {}", LocalDateTime.now());
+    }
+}
